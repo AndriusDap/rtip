c543b945b931300733746a5c6fbd9673ccc56efb
Alejandro Saucedo <axsauze@gmailcom>
2016-04-27 20:15:37 +0100

    changed sizes

--- www/app/things-to-do/overview/overview.controller.ts
+++ www/app/things-to-do/overview/overview.controller.ts
@@ -47,7 +47,7 @@ export class OverviewController {
 
                 this.events = result;
 
-                return this.setupMap();
+                // return this.setupMap();
             })
             .then(() => {
 
@@ -63,40 +63,40 @@ export class OverviewController {
         }, true);
     }
 
-    private setupMap() {
+    // private setupMap() {
 
-        var mapOptions = {
-                  zoom: 9,
-                  center: new google.maps.LatLng(53.4629985, -2.2944832),
-                  mapTypeId: google.maps.MapTypeId.ROADMAP
-              }
+    //     var mapOptions = {
+    //               zoom: 9,
+    //               center: new google.maps.LatLng(53.4629985, -2.2944832),
+    //               mapTypeId: google.maps.MapTypeId.ROADMAP
+    //           }
 
-        this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    //     this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-        var infoWindow = new google.maps.InfoWindow();
+    //     var infoWindow = new google.maps.InfoWindow();
 
-        var createMarker = (info) => {
+    //     var createMarker = (info) => {
 
-            var marker = new google.maps.Marker({
-                map: this.map,
-                position: new google.maps.LatLng(info.lat, info.long),
-                title: info.name
-            });
+    //         var marker = new google.maps.Marker({
+    //             map: this.map,
+    //             position: new google.maps.LatLng(info.lat, info.long),
+    //             title: info.name
+    //         });
 
-            marker.content = '<div class="infoWindowContent">' + info.location + '<br /></div>';
+    //         marker.content = '<div class="infoWindowContent">' + info.location + '<br /></div>';
 
-            google.maps.event.addListener(marker, 'click', () => {
-                infoWindow.setContent('<div class="map-image-container" style="background: url(' + info.image + '); background-size: cover;"></div><div><a class="marker-title" href="/#app/thingsToDo/event/' + info.id + '">' + marker.title + '</a></div>' + marker.content);
-                infoWindow.open(this.map, marker);
-            });
+    //         google.maps.event.addListener(marker, 'click', () => {
+    //             infoWindow.setContent('<div class="map-image-container" style="background: url(' + info.image + '); background-size: cover;"></div><div><a class="marker-title" href="/#app/thingsToDo/event/' + info.id + '">' + marker.title + '</a></div>' + marker.content);
+    //             infoWindow.open(this.map, marker);
+    //         });
 
-            return marker;
-        }  
+    //         return marker;
+    //     }  
 
-        for (i = 0; i < this.events.length; i++){
-            this.events[i].marker = createMarker(this.events[i]);
-        }
-    }
+    //     for (i = 0; i < this.events.length; i++){
+    //         this.events[i].marker = createMarker(this.events[i]);
+    //     }
+    // }
 
     openInfoWindow(selectedMarker) {
         var center = new google.maps.LatLng(selectedMarker.lat, selectedMarker.long);
